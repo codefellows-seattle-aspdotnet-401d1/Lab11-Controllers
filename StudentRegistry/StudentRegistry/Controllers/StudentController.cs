bc418cb6f7a4e20d0b5eb3466ea14f2ee06ff1c9
Brandon Little <brlittle86@gmail.com>
2017-09-26 07:57:39 -0700

    created basic route for StudenController

--- StudentRegistry/StudentRegistry/Controllers/StudentController.cs
+++ StudentRegistry/StudentRegistry/Controllers/StudentController.cs
@@ -7,5 +7,9 @@ namespace StudentRegistry.Controllers
 {
     public class StudentController
     {
+        public string Find(string name, int age, int course)
+        {
+            return $"{name} is a {age} year old student in a {course} course at Code Fellows.";
+        }
     }
 }
