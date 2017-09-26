f7a96b2cdeb4bea5110b3b0c4feca7083c448556
Brandon Little <brlittle86@gmail.com>
2017-09-26 07:54:43 -0700

    installed MVC dependency and created basic route for HomeController Index()

--- StudentRegistry/StudentRegistry/Controllers/HomeController.cs
+++ StudentRegistry/StudentRegistry/Controllers/HomeController.cs
@@ -1,11 +1,16 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace StudentRegistry.Controllers
 {
-    public class HomeController
+    public class HomeController : Controller
     {
+        public string Index()
+        {
+            return "Welcome to the Student Registry!";
+        }
     }
 }
