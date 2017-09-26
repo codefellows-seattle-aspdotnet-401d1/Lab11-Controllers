6b24238b746ab215a2e7dc06e497a0c7daf8f3cc
Brian McFarlane <bmcfarlane82@gmail.com>
2017-09-26 12:38:35 -0700

    Finished

---
+++ lab11_brian/lab11_brian/Controllers/ErrorController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace lab11_brian.Controllers
+{
+    [Route("Error")]
+    public class ErrorController : Controller
+    {
+        [Route("Support")]
+        public string Index()
+        {
+            return "OMG you did a bad thing .... STAP IT!";
+        }
+    }
+}
---
+++ lab11_brian/lab11_brian/Controllers/StudentsController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace lab11_brian.Controllers
+{
+    [Route("Register")]
+    public class StudentsController : Controller
+    {
+        public string Index()
+        {
+            return
+                "To begin, you must register a hacker and they will be sorted into their house. Please use: \n /Student?studentName={studentName}";
+        }
+        [Route("Student")]
+        public string Register(string studentName)
+        {
+            studentList.Add(studentName);
+            return $" Ahhh yes ... {studentName} will be placed in {SortedHouse<houses>()}!!!";
+        }
+        static void CreateFile(string filePath)
+        {
+            using (FileStream fs = System.IO.File.Create(filePath))
+            {
+                Byte[] info = new UTF8Encoding().GetBytes("");
+                fs.Write(info, 0, info.Length);
+            }
+        }
+
+        private List<string> studentList = new List<string>();
+        private T SortedHouse<T>()
+        {
+            T[] houses = (T[])Enum.GetValues(typeof(T));
+            return houses[new Random().Next(0, houses.Length)];
+        }
+        enum houses : int
+        {
+            CSharp = 1,
+            JavaScript,
+            Python
+        }
+    }
+}
